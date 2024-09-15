49efe72be738edf200b6dbf90e69e06687b1036b
SongChengCheah <112138670+Crepopcorn@users.noreply.github.com>
2024-09-15 10:37:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 session_start();
 
 
-// if already logged in, redirect to flight logs management page
+// if already login, redirect to flight logs management page
 if (isset($_SESSION['user_id'])) {
     header("Location: views/manage_flight_logs.php");
     exit();
 } else {
-    // if not logged in, redirect to login page
+    // if not login, redirect to login page
     header("Location: views/login.php");
     exit();
 }
